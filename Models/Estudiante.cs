e6a303f8df3fc1e195353675d4fbe0d4c27be082
josep <josepablo.mz04@gmail.com>
2025-10-04 20:45:24 -0600

    P-02 Tipos de tres sumas ActionName, NonAction, ActionVerbs funcionales, además  de filtros de acciones agregados en una clase propia, todo realizado por José Pablo Muñoz Zúñiga

---
+++ Models/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Trabajo_en_Clase_4.Models
+{
+    public class Estudiante
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [DisplayName("Nombre completo")]
+        public string Nombre { get; set; }
+        public int Nota { get; set; }
+
+        public Estudiante(int id, string nombre, int nota)
+        {
+            Id = id;
+            Nombre = nombre;
+            Nota = nota;
+        }
+    }
+}
--- Trabajo en Clase 4/Controllers/HomeController.cs
+++ Trabajo en Clase 4/Controllers/HomeController.cs
@@ -2,122 +2,74 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.OutputCaching;
 using System.Diagnostics;
+using Trabajo_en_Clase_4.Controllers.ActionFilters;
 using Trabajo_en_Clase_4.Models;
 
-public class HomeController : Controller
+
+namespace MvcApplication1.Controllers
 {
-    public IActionResult Index()
+    [LogActionFilter]
+    public class HomeController : Controller
     {
-        var estudiante = new Estudiante(1, "Juan Pérez", 95);
-        return View(estudiante); //default ViewData.Model
-    }
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [OutputCache(Duration = 10)]
-    public string IndexII()
-    {
-        return DateTime.Now.ToString("T"); //muestra el mismo resultado durante 10 segundos
-    }
-    public IActionResult ActionName()
-    {
-        return View();
-    }
-
-    public IActionResult NonAction()
-    {
-        return View();
-    }
-    public IActionResult ActionVerbs()
-    {
-        return View();
-    }
-
-    [ActionName("Sumar")]
-    public IActionResult Suma()
-    {
-        try
+        [Route("")] //Home
+        [Route("Indice")] // Home/Index
+        [Route("/")] //""
+        public IActionResult Index()
         {
-            int num1 = Convert.ToInt32(HttpContext.Request.Form["n1"].ToString());
-            int num2 = Convert.ToInt32(HttpContext.Request.Form["n2"].ToString());
+            var estudiante = new Estudiante(1, "Juan Pérez", 95);
+            return View(estudiante); //default ViewData.Model
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [OutputCache(Duration = 10)]
+        public string IndexII()
+        {
+            return DateTime.Now.ToString("T"); //muestra el mismo resultado durante 10 segundos
+        }
+        public IActionResult ActionName()
+        {
+            return View();
+        }
+
+        public IActionResult NonAction()
+        {
+            return View();
+        }
+        public IActionResult ActionVerbs()
+        {
+            return View();
+        }
+
+        [ActionName("Sumar")]
+        public IActionResult Sum()
+        {
+            int num1 = Convert.ToInt32(HttpContext.Request.Form["tx1"].ToString());
+            int num2 = Convert.ToInt32(HttpContext.Request.Form["tx2"].ToString());
             ViewBag.Result = (num1 + num2).ToString();
+            return View("ActionName");
         }
-        catch (Exception)
+        public string SumTemp()
         {
-            ViewBag.Result = "Datos erroneos ingresados.";
+            int num1 = Convert.ToInt32(HttpContext.Request.Form["tx1"].ToString());
+            int num2 = Convert.ToInt32(HttpContext.Request.Form["tx2"].ToString());
+            return (num1 + num2).ToString();
         }
-        return View("ActionName");
-    }
 
-
-    [NonAction]
-    public IActionResult SumTemp()
-    {
-        try
+        [HttpPost]
+        public IActionResult add3()
         {
-            int num1 = Convert.ToInt32(HttpContext.Request.Form["n1"].ToString());
-            int num2 = Convert.ToInt32(HttpContext.Request.Form["n2"].ToString());
+            int num1 = Convert.ToInt32(HttpContext.Request.Form["tx1"].ToString());
+            int num2 = Convert.ToInt32(HttpContext.Request.Form["tx2"].ToString());
             ViewBag.Result = (num1 + num2).ToString();
+            return View("ActionVerbs");
         }
-        catch (Exception)
-        {
-            ViewBag.Result = "Datos erroneos ingresados.";
-        }
-        return View("NonAction");
     }
 
-
-    [HttpPost]
-    public IActionResult Suma2()
-    {
-        try
-        {
-            int num1 = Convert.ToInt32(HttpContext.Request.Form["n1"].ToString());
-            int num2 = Convert.ToInt32(HttpContext.Request.Form["n2"].ToString());
-            ViewBag.Result = (num1 + num2).ToString();
-        }
-        catch (Exception)
-        {
-            ViewBag.Result = "Datos erroneos ingresados.";
-        }
-        return View("ActionVerbs");
-    }
 }
-
-namespace Trabajo_en_Clase_4.ActionFilters
-{
-    public class LogActionFilter : ActionFilterAttribute
-    {
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            Log("OnActionExecuting", filterContext.RouteData);
-        }
-
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            Log("OnActionExecuted", filterContext.RouteData);
-        }
-        public override void OnResultExecuting(ResultExecutingContext filterContext)
-        {
-            Log("OnResultExecuting", filterContext.RouteData);
-        }
-        public override void OnResultExecuted(ResultExecutedContext filterContext)
-        {
-            Log("OnResultExecuted", filterContext.RouteData);
-        }
-
-        private void Log(string methodName, RouteData routeData)
-        {
-            var controllerName = routeData.Values["controller"];
-            var actionName = routeData.Values["action"];
-            var message = String.Format("{0} controller:{1} action:{2}",
-           methodName, controllerName, actionName);
-            Debug.WriteLine(message, "Action Filter Log");
-        }
-       
-
-    }
-}
-
